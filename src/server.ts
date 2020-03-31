b3707333dc8f9630599419ad1b15f14d188d6c62
Brenda <brenda.nevenka.costa.bojanich@everis.com>
2020-03-31 14:13:47 -0500

    Basic Get Endpoint with Query Validation
    * Get basic endpoind added
    * Query validation added

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    let { image_url } = req.query;
 
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`Image url is required`);
+    }
+    console.info(image_url);
+    return res.status(200).send();
+  });
   //! END @TODO1
   
   // Root Endpoint
